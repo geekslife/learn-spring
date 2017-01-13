fbf1fd70a23357733e81c1aebb4722e7cc131a59
Haeyoung Kim <geekslife@gmail.com>
2017-01-14 03:21:28 +0900

    toby's spring 2-5

--- springbook/src/test/java/springbook/user/dao/UserDaoTest.java
+++ springbook/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,6 +1,7 @@
 package springbook.user.dao;
 
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.boot.test.util.ApplicationContextTestUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,30 +9,32 @@ import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
  * Created by geekslife on 2017. 1. 8..
  */
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao = context.getBean("userDao",UserDao.class);
 
         User user = new User();
-        user.setId("whiteship");
-        user.setName("BAIK");
-        user.setPassword("abc");
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
 
         dao.add( user );
 
-        System.out.println(user.getId() + " registered.");
-
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " queried.");
+        assertThat(user2.getName(), is(user.getName()));
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        JUnitCore.main("springbook.user.dao.UserDaoTest");
     }
 
 }
