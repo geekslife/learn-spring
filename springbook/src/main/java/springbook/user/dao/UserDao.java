c4bfbc90c5fae37f89787b429c2139db887d4fe1
Haeyoung Kim <geekslife@gmail.com>
2017-01-15 12:32:49 +0900

    springbook 3-20. refactored

--- springbook/src/main/java/springbook/user/dao/UserDao.java
+++ springbook/src/main/java/springbook/user/dao/UserDao.java
@@ -21,7 +21,16 @@ public class UserDao {
     private Connection c;
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        jdbcContextWithStatementStrategy(new AddStatement(user));
+        jdbcContextWithStatementStrategy(
+                c -> {
+                        PreparedStatement ps = c.prepareStatement(
+                                "insert into users(id,name,password) values(?,?,?)");
+                        ps.setString(1, user.getId());
+                        ps.setString(2, user.getName());
+                        ps.setString(3, user.getPassword());
+                        return ps;
+                }
+        );
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
@@ -58,7 +67,7 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        jdbcContextWithStatementStrategy(new DeleteAllStatement());
+        jdbcContextWithStatementStrategy( c -> c.prepareStatement("delete from users") );
     }
 
     public int getCount() throws SQLException {
